705eb38275a9306ef2da247f31fc4d11c158fb9b
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-07 06:35:22 -0600

    update file

---
+++ test/helpers.js
@@ -0,0 +1,18 @@
+import fsPromises from 'fs/promises';
+import JSDOM from 'jsdom';
+
+export const getTestTorrent = async () => {
+    const jsdom = new JSDOM.JSDOM();
+
+    return new jsdom.window.Blob([await fsPromises.readFile('./test/test.torrent')], {
+        type: 'application/x-bittorrent',
+    });
+}
+
+export const getTestHtml = async () => {
+    const jsdom = new JSDOM.JSDOM();
+
+    return new jsdom.window.Blob(['<!doctype html>'], {
+        type: 'text/html',
+    });
+}
